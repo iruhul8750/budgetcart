c2926dd7184b374e463b0be7b0db2fe93f7cc458
Ruhul Islam <iruhul8750@gmail.com>
2026-06-21 14:29:24 +0530

    fixed the mjs file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://budgetcart.com',
+  site: 'https://budgetcart.onrender.com',
   integrations: [
     tailwind(),
     sitemap(),
