450de2ebb38329052f61e4f74971157a1ca1fb1a
Ruhul Islam <iruhul8750@gmail.com>
2026-06-21 14:36:39 +0530

    Build successful - ready for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://budgetcart.onrender.com',
   integrations: [
     tailwind(),
-    sitemap(),
   ]
 });
