2695710b6e88d0eea92cea42210bb78baa5a8892
Ruhul Islam <iruhul8750@gmail.com>
2026-07-22 20:00:42 +0530

    removed keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
-import keystatic from '@keystatic/astro';
 
 export default defineConfig({
   site: 'https://budget-cart.onrender.com',
-  output: 'hybrid',
+  output: 'server',
   adapter: node({
     mode: 'standalone',
   }),
-  integrations: [
-    tailwind(),
-    keystatic({
-      config: './keystatic/config.ts',
-      route: '/keystatic',
-    }),
-  ],
+  integrations: [tailwind()],
 });
